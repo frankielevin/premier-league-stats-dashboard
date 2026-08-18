bbdff3b1ac60be918cc012d3ca5bb3e6f1f325a2
frankielevin <75389256+frankielevin@users.noreply.github.com>
2026-08-18 19:15:33 +0100

    Load enhanced opponent preview module

--- web.py
+++ web.py
@@ -13,6 +13,7 @@ def load_dashboard_modules(response):
         '<script src="/static/leaderboards.js"></script>',
         '<script src="/static/form-fixtures.js"></script>',
         '<script src="/static/standings-table.js"></script>',
+        '<script src="/static/opponent-preview.js"></script>',
     ]
     for script_tag in script_tags:
         if script_tag not in html and "</body>" in html:
